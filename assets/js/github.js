fc3a6c961ddb6f26c6b4a2066ffe2ee6c3f2d98b
Philipp Abraham <abrahamp@informatik.uni-luebeck.de>
2014-03-04 23:56:59 +0100

    small fixes

--- assets/js/github.js
+++ assets/js/github.js
@@ -6,21 +6,19 @@ jQuery.fn.loadRepositories = function(username, filterRule) {
     var type = 'all'; // Can be one of all, owner, public, private, member
     var sort = 'pushed'; // Can be one of created, updated, pushed, full_name
 
-    jQuery.getJSON('https://api.github.com/users/'+username+'/repos?type='+type+'&sort='+sort+'&callback=?').
-        done(function( response ) {
-
-            var repos = response.data.filter(filterRule);
-            var list = $('<dl/>');
-            $(repos).each(function() {
-                list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
-                list.append('<dd>' + this.description +'</dd>');
-            });
-            target.empty().append(list);
-
-        }).fail(function( jqxhr, textStatus, error ) {
-
-            var err = textStatus + ", " + error;
-            this.html("<span>Request Failed: "+err+" </span>");
-    });
+    jQuery.getJSON('https://api.github.com/users/'+username+'/repos?type='+type+'&sort='+sort+'&callback=?',
+        function( response ) {
+            if (response.meta.status !== 200) {
+                target.html("<span>Request Failed: "+response.data.message+" </span>")
+            } else {
+                var repos = response.data.filter(filterRule);
+                var list = $('<dl/>');
+                $(repos).each(function() {
+                    list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+                    list.append('<dd>' + this.description +'</dd>');
+                });
+                target.empty().append(list);
+            }
+        });
       
 };
